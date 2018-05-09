19b63d8797aa9d974ed62264491bd973bb0c8ecc
designerferro <designerferro@gmail.com>
2018-05-10 00:15:07 +0100

    Added a option to allow debugging.

--- fuel.info.sh
+++ fuel.info.sh
@@ -4,11 +4,11 @@ set -u
 set -o pipefail
 
 # Change this values acoording to instructions to match your Home-assistant
-PROTOCOL=""
-HOST_IP_OR_NAME=""
-PORT_NUMBER=""
-HAPASSWORD=""
-SHOWFUELSHOPLOCATION="NO" \
+PROTOCOL="http"
+HOST_IP_OR_NAME="localhost"
+PORT_NUMBER="8123"
+HAPASSWORD="SomePassword"
+SHOWFUELSHOPLOCATION="YES" \
 # Set this to something else like "NO" to remove from friendly names
 
 # Don't change anything bellow this line.
@@ -21,9 +21,12 @@ cat <<EOF
   Options
   -------
   h - This help text
+  d - Debugging for non-admin
   f - Get the fuel prices and publish them to your home-assistant
 
-Usage: $(basename $0) -f "nppostocombustivel nppostocombustivel nppostocombustivel"
+Usage:
+Get fuel prices > $(basename $0) -f "nppostocombustivel nppostocombustivel nppostocombustivel"
+Debug script > $(basename $0) -d "nppostocombustivel nppostocombustivel nppostocombustivel"
 
 (Enter a nppostocombustivel for each Fuel shop you want to get prices from, separeted by spaces)
 
@@ -73,7 +76,7 @@ getFuelPrices () {
     -H "Cookie:ASP.NET_SessionId=gfc3xf55hmk5dz55d0vqq0mm; mlkid=gfc3xf55hmk5dz55d0vqq0mm; Origem=MQ2; StartTime=MzksNDI2MjM4Nw2; Saida=SW5mb3JtYcOnw6NvIFPDrXRpbyBQcmXDp29zIENvbWJ1c3TDrXZlaXM1; ASPSESSIONIDAQSABRRD=EIBICCIDNJBNFDLPPNJGNEGA" \
     -H "DNT: 1" -H "Connection: keep-alive" \
     -H "Pragma: no-cache" \
-    -H "Cache-Control: no-cache" --data "tipo=popup&nppostocombustivel=$FUELSHOP" > .data
+    -H "Cache-Control: no-cache" --data "tipo=popup&nppostocombustivel=$FUELSHOP" > .data 2>&1
 
     cat .data | sed 's/||/\\\n/g' | sed 's/<[^>]*>/|/g' | sed 's/\€/EUR/g' | sed 's/||/\n/g' | sed 's/\ |//g' | sed 's/|G/G/g' | sed 's/\ EUR//g'  | sed 's/\á/a/g' | sed 's/\ó/o/g' | sed 's/\ \-\ //g' > .information
 
@@ -116,13 +119,81 @@ getFuelPrices () {
   exit 0
 }
 
-while getopts 'h:f' OPTION; do
+debugme () { 
+    #DEBUG
+    # set -x
+    SHOWFUELSHOPLOCATION="YES"
+    echo $FUELSHOPS > debugme.txt
+    for FUELSHOP in $FUELSHOPS;
+    do
+    # Read local gas and diesel prices
+    #DEBUG
+    # echo $FUELSHOP
+    curl -s "http://www.precoscombustiveis.dgeg.pt/include/infoPostoCB.aspx" \
+    -H "Host:www.precoscombustiveis.dgeg.pt" \
+    -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0" \
+    -H "Accept: */*" -H "Accept-Language: en-US,en;q=0.5" --compressed \
+    -H "Referer: http://www.precoscombustiveis.dgeg.pt/pagina.aspx?screenwidth=1280&mlkid=gfc3xf55hmk5dz55d0vqq0mm&menucb=1" \
+    -H "Content-Type: application/x-www-form-urlencoded;" \
+    -H "Cookie:ASP.NET_SessionId=gfc3xf55hmk5dz55d0vqq0mm; mlkid=gfc3xf55hmk5dz55d0vqq0mm; Origem=MQ2; StartTime=MzksNDI2MjM4Nw2; Saida=SW5mb3JtYcOnw6NvIFPDrXRpbyBQcmXDp29zIENvbWJ1c3TDrXZlaXM1; ASPSESSIONIDAQSABRRD=EIBICCIDNJBNFDLPPNJGNEGA" \
+    -H "DNT: 1" -H "Connection: keep-alive" \
+    -H "Pragma: no-cache" \
+    -H "Cache-Control: no-cache" --data "tipo=popup&nppostocombustivel=$FUELSHOP" > .data 2>&1
+
+    cat .data >> debugme.txt 
+
+    echo "==============" >> debugme.txt 
+
+    cat .data | sed 's/||/\\\n/g' | sed 's/<[^>]*>/|/g' | sed 's/\€/EUR/g' | sed 's/||/\n/g' | sed 's/\ |//g' | sed 's/|G/G/g' | sed 's/\ EUR//g'  | sed 's/\á/a/g' | sed 's/\ó/o/g' | sed 's/\ \-\ //g' > .information
+
+    cat .information >> debugme.txt
+
+    echo "==============" >> debugme.txt 
+
+    FUELSHOPLOCATION=$(cat .data | sed 's/.*infoTitulo\"><h1>//g' | sed 's/<\/h1>.*//g' | sed 's/<br>.*//g')
+    #DEBUG
+    echo $FUELSHOPLOCATION >> debugme.txt
+      # Clean the html out of this to get the values for each fuel price.
+      for LABEL in 'Gasoleo simples' 'Gasoleo\|' 'Gasoleo especial' 'Gásoleo colorido' 'Gasolina simples 95' \
+        'Gasolina 95' 'Gasolina especial 95' 'Gasolina 98' 'Gasolina especial 98' 'GPL Auto' \
+        'GNC (gas natural comprimido) - EUR/m3' 'GNC (gas natural comprimido) - EUR/kg' \
+        'GNL (gas natural liquefeito) - EUR/kg';
+      do
+        SEDLABEL=$(echo $LABEL | sed 's/\ /\\ /g' | sed 's/\//\\\//g')
+        FUELPRICE=$(awk -F"|" '/'"$SEDLABEL"'/{print $2}' .information)
+        sensorType=$(echo "$LABEL" | sed 's/\ /_/g' | sed 's~[^[:alnum:]|_]\+~~g')
+
+        if [ $SHOWFUELSHOPLOCATION = "YES" ]
+        then
+          FRIENDLYNAME="$LABEL ($FUELSHOPLOCATION)"
+        else
+          FRIENDLYNAME="$LABEL"
+        fi
+
+        SENSOR="$FUELSHOP"
+        SENSOR+="_"
+        SENSOR+="$sensorType"
+
+            if ! [ -z "$FUELPRICE" ];
+            then
+                # DEBUG
+                echo "$LABEL $FUELPRICE" >> debugme.txt
+            fi
+      done
+  done
+  exit 0
+}
+
+while getopts 'dh:f' OPTION; do
   case "$OPTION" in
     f)
       FUELSHOPS="$2"
       getFuelPrices 
       ;;
-
+    d)
+      FUELSHOPS="$2"
+      debugme
+      ;;
     h)
       usage
       ;;
